8c86d54b3e008c61798ce90dbcd83e55e4c56d5a
Ashutosh Singh Yadav <ashutoshsinghyadav7547@gmail.com>
2025-07-16 11:53:49 +0530

    Update server.js

--- server.js
+++ server.js
@@ -2,10 +2,10 @@ const express = require('express');
 const app = express();
 const PORT = process.env.PORT || 8080;
 
-app.use(express.static('public'));
+app.use(express.static('rb.html'));
 
 app.get('/', (req, res) => {
-  res.sendFile(__dirname + '/public/rb.html');
+  res.sendFile(__dirname + '/rb.html');
 });
 
 app.listen(PORT, () => {
